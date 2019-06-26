86307de2f25bad42a4231a5a3cd7637a69d838c6
Neota Moe <neota.moe@gmail.com>
2019-06-26 15:51:18 -0500

    add global cors config for use during development

--- src/main/java/com/fortybelow/backend/PlayerController.java
+++ src/main/java/com/fortybelow/backend/PlayerController.java
@@ -17,7 +17,6 @@ class PlayerController {
 //    }
 
     // Aggregate root
-    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/players")
     List<Player> all() {
         System.out.println("get all players hit me!");
